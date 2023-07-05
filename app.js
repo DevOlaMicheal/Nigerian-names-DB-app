7748bd32b0b35c046ed537eab583997a805e3018
Micheal Ojedeji <97312093+DevOlaMicheal@users.noreply.github.com>
2023-07-05 17:31:09 +0100

    Added Post request to form and currently having errors fetching by id

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
 const Name = require('./models/names')
 const app = express()
 
@@ -7,7 +8,7 @@ const app = express()
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(morgan('dev'))
-const mongoose = require('mongoose')
+app.use(express.urlencoded({ extended: true}));
 
 const uri = "mongodb+srv://oladev:ola.ejs@nodecrash.nltrmcu.mongodb.net/tndb?retryWrites=true&w=majority";
 
@@ -33,13 +34,34 @@ app.get('/about', (req, res) => {
 app.get('/names', (req, res) => {
     Name.find().sort({ createdAt: -1})
     .then(result => {
-        res.render('index', { title: 'title', names: result})
+        res.render('index', { title: 'Home', names: result})
     }).catch(err => console.log(err))
 })
+
 
 app.get('/names/create', (req, res) => {
     res.render('create', { title: "AddNew" })
 })
+
+app.get('/names/:id', (req, res) => {
+    const id = req.params.id;
+    
+    Name.findById(id)
+    .then(result => {
+        res.render('details', { title: 'name Details', name: result })
+    }).catch(err => console.log(err))
+})
+
+app.post('/names', (req, res) => {
+     const name = new Name(req.body)
+     
+     name.save().then(result => {
+        res.render('index', { title: 'Home', names: result })
+     }).catch(err => {
+        console.log(err)
+     })
+})
+
 
 app.use((req, res) => {
     res.status(404).render('404', { title: "Error-404" })
